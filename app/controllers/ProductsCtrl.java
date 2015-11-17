aecf69b532b278456f04d319944264a713c8060e
ncphillips <ncphillips.19@gmail.com>
2015-11-17 10:37:19 -0400

    We now have http routes for listing, viewing, creating, and saving products, along with matching Action controller methods.

---
+++ app/controllers/ProductsCtrl.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class ProductsCtrl extends Controller {
+    public static Result list() {
+        return TODO;
+    }
+
+    public static Result newProduct() {
+        return TODO;
+    }
+
+    public static Result view(String ean) {
+        return TODO;
+    }
+
+    public static Result save() {
+        return TODO;
+    }
+}
