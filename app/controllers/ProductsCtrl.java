51833357487738df9baf2777f53467ff96ea7f73
ncphillips <ncphillips.19@gmail.com>
2015-11-17 10:59:32 -0400

    The client can now view a form for creating a new Product in the collection.

--- app/controllers/ProductsCtrl.java
+++ app/controllers/ProductsCtrl.java
@@ -1,21 +1,34 @@
 package controllers;
 
-import collections.ProductCollection;
-import models.Product;
-import views.html.products.list;
-import play.mvc.Controller;
-import play.mvc.Result;
-
+// Utils
 import java.util.List;
 
+// Play Toys
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.data.Form;
+
+// Collections
+import collections.ProductCollection;
+
+// Modelsj
+import models.Product;
+
+// Views
+import views.html.products.list;
+import views.html.products.view;
+
 public class ProductsCtrl extends Controller {
+    private static final Form<Product> productForm = Form.form(Product.class);
+
     public static Result list() {
         List<Product> products = ProductCollection.findAll();
         return ok(list.render(products));
     }
 
     public static Result newProduct() {
-        return TODO;
+
+        return ok(view.render(productForm));
     }
 
     public static Result view(String ean) {
