428122eecab8f2bc1d9b986bb0f59361be836642
ncphillips <ncphillips.19@gmail.com>
2015-11-17 10:45:21 -0400

    The client can now view a list of all products in the collectoin.

--- app/controllers/ProductsCtrl.java
+++ app/controllers/ProductsCtrl.java
@@ -1,11 +1,17 @@
 package controllers;
 
+import collections.ProductCollection;
+import models.Product;
+import views.html.products.list;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import java.util.List;
+
 public class ProductsCtrl extends Controller {
     public static Result list() {
-        return TODO;
+        List<Product> products = ProductCollection.findAll();
+        return ok(list.render(products));
     }
 
     public static Result newProduct() {
