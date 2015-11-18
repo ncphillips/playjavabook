71357dd9c306cc8ffa4b4fac3c216339f3acb9ed
ncphillips <ncphillips.19@gmail.com>
2015-11-18 09:41:19 -0400

    We can now add and view Tags on a Product.

--- app/controllers/ProductsCtrl.java
+++ app/controllers/ProductsCtrl.java
@@ -1,9 +1,11 @@
 package controllers;
 
 // Utils
+import java.util.ArrayList;
 import java.util.List;
 
 // Play Toys
+import models.Tag;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.data.Form;
@@ -20,6 +22,10 @@ import views.html.products.view;
 
 public class ProductsCtrl extends Controller {
     private static final Form<Product> productForm = Form.form(Product.class);
+
+    public static Result index() {
+        return redirect(routes.ProductsCtrl.list(0));
+    }
 
     public static Result list(Integer page) {
         List<Product> products = ProductCollection.findAll();
@@ -48,16 +54,24 @@ public class ProductsCtrl extends Controller {
         }
 
         Product product = boundForm.get();
+        List<Tag> tags = new ArrayList<Tag>();
+
+        product.tags.stream()
+                .filter(tag -> tag != null)
+                .map(tag -> Tag.findById(tag.id))
+                .forEach(tags::add);
+
+        product.tags = tags;
         ProductCollection.save(product);
         flash("success", String.format("Successfully added product %s.", product));
-        return redirect(routes.ProductsCtrl.list());
+        return redirect(routes.ProductsCtrl.list(0));
     }
 
     public static Result delete(String ean) {
         Product product = ProductCollection.findByEan(ean);
         if (product != null) {
             ProductCollection.remove(product);
-            return redirect(routes.ProductsCtrl.list());
+            return redirect(routes.ProductsCtrl.list(0));
         }
         return notFound();
     }
